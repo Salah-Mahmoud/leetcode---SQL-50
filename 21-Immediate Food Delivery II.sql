c9806cb321e45ceb830bdfe7619cc5bf2d48c461
Salah Mahmoud <124273567+Salah-Mahmoud@users.noreply.github.com>
2024-11-05 11:05:32 +0200

    21 edit names

--- 21-Immediate Food Delivery II.sql
+++ 21-Immediate Food Delivery II.sql
@@ -2,7 +2,7 @@ WITH RankedOrders AS (
     SELECT
         customer_id,
         order_date,
-        preferred_delivery_date,
+        customer_pref_delivery_date,
         DENSE_RANK() OVER (
             PARTITION BY customer_id
             ORDER BY order_date
@@ -11,6 +11,6 @@ WITH RankedOrders AS (
 )
 
 SELECT
-    ROUND(AVG(CASE WHEN order_date = preferred_delivery_date THEN 1 ELSE 0 END) * 100, 2) AS on_time_first_order_percentage
+    ROUND(AVG(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) * 100, 2) AS immediate_percentage
 FROM RankedOrders
 WHERE order_rank = 1;
