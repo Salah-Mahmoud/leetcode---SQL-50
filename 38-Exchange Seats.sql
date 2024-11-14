17b8c5d39cc1c3fd6c791c25a7cb497d957f9ed7
Salah Mahmoud <124273567+Salah-Mahmoud@users.noreply.github.com>
2024-11-14 12:24:46 +0200

    38 & 44 Q

---
+++ 38-Exchange Seats.sql
@@ -0,0 +1,9 @@
+select
+    case
+        when id = (select max(id) from Seat) and id %2 != 0 then id
+        when id %2 = 0 then id-1
+        when id %2 != 0 then id+1
+    end as id,
+    student
+from Seat
+order by id
