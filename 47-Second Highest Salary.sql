45ba85c6c6275f055ad1b7bdd3366d2622ad30c2
Salah Mahmoud <124273567+Salah-Mahmoud@users.noreply.github.com>
2024-11-18 10:06:01 +0200

    40 & edit 47 Q

--- 47-Second Highest Salary.sql
+++ 47-Second Highest Salary.sql
@@ -1,19 +1,14 @@
-WITH rank_second_salary AS (
-    SELECT
-        salary AS SecondHighestSalary,
-        RANK() OVER (ORDER BY salary DESC) AS Salary_rank
-    FROM Employee
+with cte_e as (
+select  id,
+        salary,
+        DENSE_RANK() OVER (
+		ORDER BY salary desc
+	    ) dense_rank_number
+from Employee 
 )
-SELECT
-    SecondHighestSalary
-FROM
-    rank_second_salary
-WHERE Salary_rank = 2
-UNION ALL
-SELECT
-    NULL
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM rank_second_salary
-    WHERE Salary_rank = 2
-);
+SELECT 
+    CASE 
+        WHEN EXISTS (SELECT 1 FROM cte_e WHERE dense_rank_number = 2) 
+        THEN (SELECT salary FROM cte_e WHERE dense_rank_number = 2 limit 1)
+        ELSE NULL
+    END AS SecondHighestSalary;
